de98ddf8c81968f07abc8c54d98dbe139b955ead
bovenson <szhkai@qq.com>
2017-10-14 16:27:23 +0800

    auto commit

---
+++ Algorithm/Introduction_To_Algorithm_Competition/Book1/Solution001.java
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: Solution001.java
+	> Author: bovenson
+	> Email:  szhkai@126.com
+	> Created Time: 2017-10-14 15:18:01
+ ************************************************************************/
+
+public class Solution001 {
+	public static void main(String args[]) {
+		System.out.println("Hello World");
+	}
+}
+
