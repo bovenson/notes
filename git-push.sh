e1ee54f52b8d3e866f53bc76d7e644cd2cb33676
bovenson <szhkai@qq.com>
2017-10-21 11:11:29 +0800

    修改git-push.sh

--- git-push.sh
+++ git-push.sh
@@ -4,7 +4,14 @@
 FULLPATH="$PWD/$(dirname $0)"
 cd $FULLPATH
 
+if [ $1 ]
+then
+    comment=$1
+else
+    comment="auto commit"
+fi  
+
 git add --all
-git commit -m "auto commit"
+git commit -m $comment
 git push origin master
 git push mayun master
