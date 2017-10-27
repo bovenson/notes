93edccad1e58939fb5836e9c4e03f6156aca7bc0
bovenson <szhkai@qq.com>
2017-10-27 19:25:45 +0800

    auto commit

--- git-push.sh
+++ git-push.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+# @author bovenson
+# @description git repo (notes) auto commit
+
+
 # echo `pwd` > /home/bovenson/Tmp/cron.log
 
+
+# setting pwd
 FULLPATH="$PWD/$(dirname $0)"
 cd $FULLPATH
 
+# setting comment
 if [ $1 ]
 then
     comment=$1
@@ -11,6 +18,33 @@ else
     comment="auto commit"
 fi  
 
+GITHUB_ADDR="git@github.com:bovenson/notes.git"
+MAYUN_ADDR="git@gitee.com:bovenson/notes.git"
+
+# remote_origin_exits=`git remote -v | grep "^origin"`
+# remote_mayun_exits=`git remote -v | grep "^mayun"`
+# echo $remote_origin_exits
+# echo $remote_mayun_exits
+
+if [ "`git remote -v | grep '^origin'`" ]
+then
+	echo "origin exits"
+else
+	echo "origin not exits"
+	eval "git remote add origin '$ORIGIN_ADDR'"
+	echo "added origin $ORIGIN_ADDR"
+fi
+
+if [ "`git remote -v | grep '^mayun'`" ]
+then
+	echo "mayun exits"
+else
+	echo "mayun not exits"
+	eval "git remote add origin '$MAYUN_ADDR'"
+	echo "added mayun $ORIGIN_ADDR"
+fi
+
+# exit
 git add --all
 git commit -m "$comment"
 git push origin master
