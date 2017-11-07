ffd7d080930d1d398d476e0b9be9c3d4e984c675
bovenson <szhkai@qq.com>
2017-11-07 19:17:22 +0800

    auto commit

---
+++ Algorithm/Classics/linked_list/copy_random_pointer.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+# coding: utf-8
+
+"""
+A linked list is given such that each node contains an additional random
+pointer which could point to any node in the list or None.
+
+Return the deep copy of this list.
+"""
+
+__author__ = "bovenson"
+__email__ = "szhkai@qq.com"
+__date__ = "2017-11-07 10:02"
+
+
+class Node:
+    def __init__(self):
+        self.data = None
+        self.next = None
+        self.random = None
+
+
+def deep_copy_linked_list(rll):
+    """深复制带有随机指针的链表"""
+    
+    pass
+
+
+if __name__ == "__main__":
+    pass
---
+++ Python/Notes/2diff3.md
@@ -0,0 +1,17 @@
+---
+title: Python2和Python3差异
+tags: Python
+---
+
+# print
+
+Python3 中`print`不再是语句, 而是函数
+
+```python
+#### python2
+print 'abc'
+
+#### python3
+print('abc')
+```
+
