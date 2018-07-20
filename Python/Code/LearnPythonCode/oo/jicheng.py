59f9bd06a9b31fc3555b4adf8b34ca74c31b7e2c
bovenson <szhkai@qq.com>
2018-07-20 11:04:00 +0800

    auto commit

---
+++ Python/Code/LearnPythonCode/oo/jicheng.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+# coding: utf-8
+
+"""
+FILE: 
+DESC: 
+"""
+
+__author__ = "bovenson"
+__email__ = "szhkai@qq.com"
+__date__ = "2018-07-20 10:51"
+
+
+class A:
+    def f(self):
+        print('A.f()')
+
+
+class B:
+    def f(self):
+        print('B.f()')
+
+
+class C(A, B):
+    pass
+
+
+c = C()
+c.f()
+
+cs = C()
+super(C, cs).f()
