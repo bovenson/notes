913d0e1bfab52353d1d7353b218c31a8a07edc89
sunzhenkai <szhkai@qq.com>
2019-01-16 20:46:45 +0800

    auto commit

--- Python/Matplotlib/Code/subplot/subplot02.py
+++ Python/Matplotlib/Code/subplot/subplot02.py
@@ -4,6 +4,16 @@
 @author	sunzhenkai
 @file	subplot02.py
 @date	2019-01-16 09:20:20
-@desc	
+@desc
 """
 
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 2*np.pi, 100)
+xx = np.linspace(0, 2*np.pi + 0.5, 100)
+y = 2*np.sin(x)
+plt.plot(x, label="x")
+plt.plot(xx, label="xx")
+plt.legend()
+plt.show()
