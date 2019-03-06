e397cf4bf05d875abe5bbec5a5ac6675b4826d24
sunzhenkai <szhkai@qq.com>
2019-03-06 10:00:58 +0800

    update

--- Scala/Code/Learn/003.scala
+++ Scala/Code/Learn/003.scala
@@ -1,6 +1,14 @@
-val f = Future {
-    println("lcs")
+import scala.concurrent.{Await, ExecutionContext, Future, TimeoutException}
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+
+
+val f = Future[Int] {
+    Thread.sleep(1000)
+    1 + 1
 }
 
-
-
+// 3 - this is blocking (blocking is bad)
+val result = Await.result(f, 1 second)
+println(result)
